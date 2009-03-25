93fa4c5d18e595095ee5836419ee10be928f11f8
vadim.manikhin <vadim.manikhin@51a878ca-17b7-11de-9d52-39b120432c5d>
2009-03-25 13:05:07 +0000

    smarty, ext css

---
+++ trunk/php/templates/templates/include_js.tpl
@@ -0,0 +1,4 @@
+{foreach from=$js_scripts item=js_script}
+    <script type="text/javascript" src="../js/{$js_script}"></script>
+{/foreach}
+
---
+++ trunk/php/templates/templates_c/%%95^959^959BC755%%body.tpl.php
@@ -0,0 +1,17 @@
+<?php /* Smarty version 2.6.22, created on 2009-03-25 04:49:45
+         compiled from body.tpl */ ?>
+<body>
+    <?php $_from = $this->_tpl_vars['bodies']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
+    foreach ($_from as $this->_tpl_vars['body']):
+?>
+        <?php if ($this->_tpl_vars['body'] == 'chains'): ?>
+            <?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "body_chains.tpl", 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+        <?php endif; ?>
+    <?php endforeach; endif; unset($_from); ?>
+</body>
+</html>
+
