d06f441f6cf0455deb549d5a8687b5cb8a34e1b1
vadim.manikhin <vadim.manikhin@51a878ca-17b7-11de-9d52-39b120432c5d>
2009-03-31 17:35:29 +0000

    jquery ui css was added.

--- trunk/php/chains.php
+++ trunk/php/chains.php
@@ -4,7 +4,7 @@
     $smarty = new SmartySilkRoadACP();
     $smarty->assign("app_page_name", "Chains");
     $smarty->assign("js_scripts", array("jquery-1.3.2.min.js", "jquery-ui-1.7.1.custom.min.js", "global.js", "init_and_events.js", "events_handlers.js"));
-    $smarty->assign("css_files", array("chains.css", "global.css"));
+    $smarty->assign("css_files", array("jquery-ui-1.7.1.custom.css", "chains.css", "global.css"));
     $smarty->display("header.tpl");
     $smarty->assign("sr_login", $_SESSION["silkroad_login"]);
     $smarty->assign("sr_host", $_SESSION["silkroad_host"]);
