55154c52117f72d168c06aed9efcd23d67fb4821
vadim.manikhin <vadim.manikhin@51a878ca-17b7-11de-9d52-39b120432c5d>
2009-03-31 14:55:12 +0000

    new menu in main menu.

--- php/templates/templates_c/%%86^865^865468CA%%body_main_menu.tpl.php
+++ php/templates/templates_c/%%86^865^865468CA%%body_main_menu.tpl.php
@@ -1,10 +1,11 @@
-<?php /* Smarty version 2.6.22, created on 2009-03-31 01:59:32
+<?php /* Smarty version 2.6.22, created on 2009-03-31 07:24:00
          compiled from body_main_menu.tpl */ ?>
 <div id="main_menu">
     <div id="session_data_and_logout"><?php echo $this->_tpl_vars['sr_login']; ?>
 @<?php echo $this->_tpl_vars['sr_host']; ?>
  | <a href="./chains.php?action=logout">Logout</a></div>
     <div id="main_menu_chains" class="main_menu_element">Chains</div>
+    <div id="main_menu_executed_tcs" class="main_menu_element">Executed TCs</div>
     <div class="clear"></div>
 </div>
 
