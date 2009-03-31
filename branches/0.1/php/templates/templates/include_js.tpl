02d041cf69d1f57d76391b1b38567f0fd875c696
vadim.manikhin <vadim.manikhin@51a878ca-17b7-11de-9d52-39b120432c5d>
2009-03-31 10:29:19 +0000

    branch will be renamed.

--- branches/0.1/php/templates/templates/include_js.tpl
+++
@@ -1,4 +0,0 @@
-{foreach from=$js_scripts item=js_script}
-    <script type="text/javascript" src="../js/{$js_script}"></script>
-{/foreach}
-
--- branches/0.1/php/templates/templates_c/%%31^316^316D3695%%include_js.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.22, created on 2009-03-30 00:09:51
-         compiled from include_js.tpl */ ?>
-<?php $_from = $this->_tpl_vars['js_scripts']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
-    foreach ($_from as $this->_tpl_vars['js_script']):
-?>
-    <script type="text/javascript" src="../js/<?php echo $this->_tpl_vars['js_script']; ?>
-"></script>
-<?php endforeach; endif; unset($_from); ?>
-
