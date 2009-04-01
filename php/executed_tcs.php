4c18f3f3064a7cab21709c725b10546c3c0ad898
vadim.manikhin <vadim.manikhin@51a878ca-17b7-11de-9d52-39b120432c5d>
2009-04-01 14:18:00 +0000

    php

--- php/executed_tcs.php
+++ php/executed_tcs.php
@@ -4,7 +4,7 @@
     $smarty = new SmartySilkRoadACP();
     $smarty->assign("app_page_name", "Executed Test Cases");
     $smarty->assign("js_scripts", array("jquery-1.3.2.min.js", "jquery-ui-1.7.1.custom.min.js", "global.js", "executed_tcs.js"));
-    $smarty->assign("css_files", array("jquery-ui-1.7.1.custom.css", "executed_tcs.css", "global.css"));
+    $smarty->assign("css_files", array("jquery-ui-1.7.1.custom.css", "executed_tcs.css", "chains.css", "global.css"));
     $smarty->display("header.tpl");
     $smarty->assign("sr_login", $_SESSION["silkroad_login"]);
     $smarty->assign("sr_host", $_SESSION["silkroad_host"]);
