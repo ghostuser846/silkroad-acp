5372a25ba607b00cb3135d9e17ff9d0545cd25ef
vadim.manikhin <vadim.manikhin@51a878ca-17b7-11de-9d52-39b120432c5d>
2009-04-01 14:17:33 +0000

    changes which reflect addition of new class Queries.

--- trunk/php/get_test_chains.php
+++ trunk/php/get_test_chains.php
@@ -1,54 +1,23 @@
 <?php
     require_once("./choose_db.php");
+    require_once("./queries.php");
     //error_reporting(E_ALL);
     header("Content-type: text/xml");
     header("Cache-Control: no-cache");
     $db_host = $_SESSION["silkroad_host"];
     $db_user = $_SESSION["silkroad_login"];
     $db_pwd = $_SESSION["silkroad_pass"];
-    $database = "silkroad";
-    $table_tpnames = "testplan_names";
-    $table_testchains = "testchains";
-    $table_machines = "machines";
-    $table_configurations = "configurations";
-    $table_testplans = "testplans";
-    $table_testcases = "testcases";
-    $table_tcchains = "tcchains";
 
+    $queries = new Queries();
     $link = mysql_connect($db_host, $db_user, $db_pwd)
         or die("Can't connect to DB: " . mysql_error());
     foreach ($_POST as $key => $value)
         $$key = mysql_real_escape_string($value, $link);
-    mysql_select_db($database, $link) or die("Can't select database: " . mysql_error());
+    mysql_select_db($queries->getDataBase(), $link) or die("Can't select database: " . mysql_error());
+    
     if ($action == "get_chains") {
-        $query_chains_not_running = "select tc.id, tpn.name, m.name, c.name, tc.is_completed
-                                    from {$table_testchains} tc, {$table_machines} m, {$table_tpnames} tpn,  {$table_configurations} c
-                                        where tc.machineid = m.id
-                                        and tc.testplanid = tpn.id
-                                        and tc.configid = c.id
-                                        and (tc.machineid, tc.testplanid) not in
-                                            (select distinct first_join.machineid, {$table_testplans}.testplanid
-                                            from {$table_testplans} join
-                                                    (select {$table_testcases}.id, {$table_tcchains}.machineid
-                                                    from {$table_tcchains} join {$table_testcases}
-                                                    on {$table_tcchains}.testcase = {$table_testcases}.testcase) first_join
-                                                on {$table_testplans}.testcaseid = first_join.id)
-                                                order by tc.id asc;";
-        $query_chains_running = "select tc.id, tpn.name, m.name, c.name, tc.is_completed
-                                    from {$table_testchains} tc, {$table_machines} m, {$table_tpnames} tpn,  {$table_configurations} c
-                                        where tc.machineid = m.id
-                                        and tc.testplanid = tpn.id
-                                        and tc.configid = c.id
-                                        and (tc.machineid, tc.testplanid) in
-                                            (select distinct first_join.machineid, {$table_testplans}.testplanid
-                                            from {$table_testplans} join
-                                                    (select {$table_testcases}.id, {$table_tcchains}.machineid
-                                                    from {$table_tcchains} join {$table_testcases}
-                                                    on {$table_tcchains}.testcase = {$table_testcases}.testcase) first_join
-                                                on {$table_testplans}.testcaseid = first_join.id)
-                                    order by tc.id asc;";
-        $result = mysql_query($query_chains_not_running, $link);
-        $result_running = mysql_query($query_chains_running, $link);
+        $result = mysql_query($queries->getNotRunningChains(), $link);
+        $result_running = mysql_query($queries->getRunningChains(), $link);
         if (!$result || !$result_running)
             die("Query to show fields from table failed: " . mysql_error());
         echo "<?xml version=\"1.0\"?>\n";
@@ -85,15 +54,15 @@
         foreach ($arr as $i => $value) {
             switch ($value) {
                 case "Machines": {
-                    $result = mysql_query("select id, name from {$table_machines};", $link);
+                    $result = mysql_query($queries->getMachines(), $link);
                     break;
                 }
                 case "TestPlans": {
-                    $result = mysql_query("select id, name from {$table_tpnames};", $link);
+                    $result = mysql_query($queries->getTestPlans(), $link);
                     break;
                 }
                 case "Configurations": {
-                    $result = mysql_query("select id, name from {$table_configurations};", $link);
+                    $result = mysql_query($queries->getConfigurations(), $link);
                     break;
                 }
             }
@@ -112,9 +81,7 @@
         mysql_free_result($result);
     }
     if ($action == "add_chain") {
-        $result = mysql_query("insert into {$table_testchains} 
-            (machineID, testplanID, configID, is_completed) 
-            values ({$machine}, {$testplan}, {$config}, {$iscompleted});", $link);
+        $result = mysql_query($queries->getInsertNewChain($machine, $testplan, $config, $iscompleted), $link);
         echo "<?xml version=\"1.0\"?>\n";
         echo "<RootElement>\n";
         if (!$result) {
@@ -127,7 +94,7 @@
         echo "</RootElement>\n";
     }
     if ($action == "delete_all") {
-        $result = mysql_query("truncate {$table_testchains};", $link);
+        $result = mysql_query($queries->getTruncateTestChains(), $link);
         echo "<?xml version=\"1.0\"?>\n";
         echo "<RootElement>\n";
         if (!$result) {
@@ -140,15 +107,7 @@
         echo "</RootElement>\n";
     }
     if ($action == "delete_not_running") {
-        $query = "delete from {$table_testchains}
-                    where ({$table_testchains}.machineid, {$table_testchains}.testplanid) not in
-                        (select distinct first_join.machineid, {$table_testplans}.testplanid
-                        from {$table_testplans} join
-                                (select {$table_testcases}.id, {$table_tcchains}.machineid
-                                from {$table_tcchains} join {$table_testcases}
-                                    on {$table_tcchains}.testcase = {$table_testcases}.testcase) first_join
-                            on {$table_testplans}.testcaseid = first_join.id);";
-        $result = mysql_query($query, $link);
+        $result = mysql_query($queries->getDeleteNotRunningChains(), $link);
         echo "<?xml version=\"1.0\"?>\n";
         echo "<RootElement>\n";
         if (!$result) {
@@ -161,7 +120,7 @@
         echo "</RootElement>\n";
     }
     if ($action == "delete_following") {
-        $result = mysql_query("delete from {$table_testchains} where id in ({$chains});", $link);
+        $result = mysql_query($queries->getDeleteFollowingChains($chains), $link);
         echo "<?xml version=\"1.0\"?>\n";
         echo "<RootElement>\n";
         if (!$result) {
@@ -186,15 +145,7 @@
         // explode POST data (new sequence of ids)
         $new_rotation_arr = explode(",", $rotations);
         
-        $query = "select * from {$table_testchains}
-                    where ({$table_testchains}.machineid, {$table_testchains}.testplanid) not in
-                        (select distinct first_join.machineid, {$table_testplans}.testplanid
-                        from {$table_testplans} join
-                                (select {$table_testcases}.id, {$table_tcchains}.machineid
-                                from {$table_tcchains} join {$table_testcases}
-                                    on {$table_tcchains}.testcase = {$table_testcases}.testcase) first_join
-                            on {$table_testplans}.testcaseid = first_join.id);";
-        $result = mysql_query($query, $link);
+        $result = mysql_query($queries->GetAllTestChainsData(), $link);
         if (!$result) {
             send_xml_fail_and_exit();
         }
@@ -216,10 +167,9 @@
             // so, rotate them
             // former id ($key) should contain new data from $value id
             if ($key != $value) {
-                $query = "update {$table_testchains} set machineid = {$current_content[$value][1]}, 
-                    testplanid = {$current_content[$value][2]}, configid = {$current_content[$value][3]}, 
-                    is_completed = {$current_content[$value][4]} where id = {$key};";
-                $result = mysql_query($query, $link);
+                $result = mysql_query($queries->getUpdateChain($current_content[$value][1],
+                    $current_content[$value][2], $current_content[$value][3],
+                    $current_content[$value][4], $key), $link);
                 if (!$result)
                     send_xml_fail_and_exit();
             }
