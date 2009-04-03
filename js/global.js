d0dee30d8f8c092985b0631b54121177d552c2da
vadim.manikhin <vadim.manikhin@51a878ca-17b7-11de-9d52-39b120432c5d>
2009-04-03 14:23:15 +0000

    comments added.

--- js/global.js
+++ js/global.js
@@ -1,10 +1,12 @@
 $(document).ready(function(e) {
+    // Highlight focused button in main menu
     $("#main_menu div.main_menu_element").bind("mouseenter", function(e) {
         $(this).css("background-color", "green");
     });
     $("#main_menu div.main_menu_element").bind("mouseleave", function(e) {
         $(this).css("background-color", "white");
     });
+    // Main menu click handlers
     $("#main_menu div.main_menu_element").bind("click", function(e) {
         switch ($(this).text()) {
             case "Chains": {
@@ -21,6 +23,7 @@ $(document).ready(function(e) {
             }
         }
     });
+    // Highlight focused "action" button in other menu (eg in Search panel)
     $("div.button_action").bind("mouseenter", function(e) {
         $(this).css("background-color", "red");
     });
