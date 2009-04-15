af24e13abebd1038f5e9abcf4967d6f3f4f8c278
vadim.manikhin <vadim.manikhin@51a878ca-17b7-11de-9d52-39b120432c5d>
2009-04-15 14:03:13 +0000

    recompiled.

--- php/templates/templates_c/%%86^865^865468CA%%body_main_menu.tpl.php
+++ php/templates/templates_c/%%86^865^865468CA%%body_main_menu.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.22, created on 2009-03-31 07:24:00
+<?php /* Smarty version 2.6.22, created on 2009-04-14 04:19:38
          compiled from body_main_menu.tpl */ ?>
 <div id="main_menu">
     <div id="session_data_and_logout"><?php echo $this->_tpl_vars['sr_login']; ?>
@@ -6,6 +6,6 @@
  | <a href="./chains.php?action=logout">Logout</a></div>
     <div id="main_menu_chains" class="main_menu_element">Chains</div>
     <div id="main_menu_executed_tcs" class="main_menu_element">Executed TCs</div>
+    <div id="main_menu_run_transporter" class="main_menu_element">Run Transporter</div>
     <div class="clear"></div>
-</div>
-
+</div>
--- php/templates/templates_c/%%95^959^959BC755%%body.tpl.php
+++ php/templates/templates_c/%%95^959^959BC755%%body.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.22, created on 2009-03-31 07:24:27
+<?php /* Smarty version 2.6.22, created on 2009-04-14 04:19:27
          compiled from body.tpl */ ?>
 <body>
     <?php $_from = $this->_tpl_vars['bodies']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
@@ -30,6 +30,14 @@ unset($_smarty_tpl_vars);
             <div id="body_keeper">
             <?php $_smarty_tpl_vars = $this->_tpl_vars;
 $this->_smarty_include(array('smarty_include_tpl_file' => "body_executed_tcs.tpl", 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+        <?php endif; ?>
+        <?php if ($this->_tpl_vars['body'] == 'run_transporter'): ?>
+            <div id="body_keeper">
+            <?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "body_run_transporter.tpl", 'smarty_include_vars' => array()));
 $this->_tpl_vars = $_smarty_tpl_vars;
 unset($_smarty_tpl_vars);
  ?>
